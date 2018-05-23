44b82e2e4f38590205e6f003b8f133e9c15082a6
Wojciech Rebis <wojciech.rebis@mediaregionalne.pl>
2018-05-24 00:09:13 +0200

    Login box styles

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatCardModule,
   ],
 })
 export class AppMaterialModule { }
