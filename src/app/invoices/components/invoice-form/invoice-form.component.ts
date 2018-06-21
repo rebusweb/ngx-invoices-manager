a9a3a84c2fce8cc35f20281f766241d169ecd711
Wojciech Rębiś <wojtek@nitro-digital.com>
2018-06-21 11:44:35 +0200

    Product form array

--- src/app/invoices/components/invoice-form/invoice-form.component.ts
+++ src/app/invoices/components/invoice-form/invoice-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Invoice } from '../../../shared/models/invoice';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-form',
@@ -9,12 +9,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class InvoiceFormComponent implements OnInit {
 
-  invoice: FormGroup;
+  invoiceForm: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.invoice = this.fb.group({
+    this.invoiceForm = this.fb.group({
       supplier: this.fb.group({
         name: ['', Validators.required],
         street: ['', Validators.required],
@@ -29,17 +29,29 @@ export class InvoiceFormComponent implements OnInit {
         city: ['', Validators.required],
         nip: ['', Validators.required],
       }),
-      products: this.fb.group({
-        name: ['', Validators.required],
-        count: ['', Validators.required],
-        price: ['', Validators.required],
-      }),
+      products: this.fb.array([ this.createProduct() ]),
       paymentType: ['', Validators.required],
       paymentTime: ['', Validators.required],
     });
   }
 
-  submit({ value, valid }: { value: Invoice, valid: boolean }) {
+  get products(): FormArray {
+    return this.invoiceForm.get('products') as FormArray;
+  }
+
+  createProduct(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      count: ['', Validators.required],
+      price: ['', Validators.required],
+    });
+  }
+
+  addProduct(): void {
+    this.products.push(this.createProduct());
+  }
+
+  submit({ value, valid }: { value: Invoice, valid: boolean }): void {
     console.log(value, valid);
   }
 
